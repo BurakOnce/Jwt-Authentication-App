c88df57759a0d7594119959958adaa29b0319c31
Burak Önce <119293638+BurakOnce@users.noreply.github.com>
2024-02-04 00:08:38 +0300

    Update UserRepository.java

--- Jwt-Authentication/src/main/java/com/example/jwtauthentication/repository/UserRepository.java
+++ Jwt-Authentication/src/main/java/com/example/jwtauthentication/repository/UserRepository.java
@@ -3,10 +3,14 @@ package com.example.jwtauthentication.repository;
 import com.example.jwtauthentication.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String userName);
+
+    List<User> findUsersByCity(String city);
 
 }
